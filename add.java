a6e0bca4ee524e3b2038929be2edb9347510c72d
nehanalinik <nehanalinik@gmail.com>
2021-07-18 11:02:52 +0530

    added msg in add

--- add.java
+++ add.java
@@ -3,6 +3,6 @@ public class add {
         int a = 10, b = 20;
         int c;
         c = a + b;
-        System.out.printf("Addition of two numbers is : " + c);
+        System.out.printf("Addition is : " + c);
     }
 }
