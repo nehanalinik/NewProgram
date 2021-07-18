b4c7c0289b216ad1c69233e8c234c9304e3a407a
nehanalinik <nehanalinik@gmail.com>
2021-07-18 10:53:32 +0530

    initial commit

---
+++ add.java
@@ -0,0 +1,8 @@
+public class add {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int c;
+        c = a + b;
+        System.out.printf("Addition of two numbers is : " + c);
+    }
+}
